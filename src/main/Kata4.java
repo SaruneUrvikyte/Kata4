560a734d351a65183300606bca23dd451009aabb
SaruneUrvikyte <sarune.urvikyte@gmail.com>
2018-11-11 13:54:13 +0000

    Kata4 v2

--- src/main/Kata4.java
+++ src/main/Kata4.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.io.IOException;
 import java.util.List;
 import model.Histogram;
 import model.Mail;
@@ -17,14 +18,31 @@ public class Kata4 {
     private static List <Mail> mailList;
     private static Histogram <String> histogram;
     private static HistogramDisplay histoDisplay;
+    private static MailListReader mLR = new MailListReader();
+    private static MailHistogramBuilder mHB = new MailHistogramBuilder();
+        
+    public static void main(String[] args) throws Exception{
+        
+        Kata4 kata4 = new Kata4();
+        kata4.control();   
+    }
     
-    public static void main(String[] args) {
-        MailListReader mLR = new MailListReader();
-        MailHistogramBuilder mHB = new MailHistogramBuilder();
+    private void control() throws IOException{
+        input();
+        process();
+        output();
+    }
+    
+    private void input(){
         mailList = mLR.read(fileName);
+    }
+    
+    private void process(){
         histogram = mHB.build(mailList);
+    }
+    
+    private void output(){
         histoDisplay = new HistogramDisplay(histogram);
         histoDisplay.execute();
     }
-    
 }
